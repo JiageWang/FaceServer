96087b4ad6cccdda6ac23b084335d970e6b596e6
JiageWang <1076050774@qq.com>
2019-06-26 11:12:16 +0800

    add embedding

---
+++ app/models/embedding.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, BLOB
+from . import db
+
+
+class Embedding(db.Model):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    staff = db.relationship('Staff', backref=db.backref('embedding'))
+    sid = Column(Integer, db.ForeignKey('staff.id'), unique=True)
+    emd = Column(BLOB)
